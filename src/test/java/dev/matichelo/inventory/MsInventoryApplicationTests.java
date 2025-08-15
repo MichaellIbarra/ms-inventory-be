295d105884d3be52bd363602e45b126746db4b90
MichaellIbarra <michaell.ibarra@vallegrande.edu.pe>
2025-08-14 19:23:15 -0500

    chore(inventory): inizialize ms-inventory backend

---
+++ src/test/java/dev/matichelo/inventory/MsInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.matichelo.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsInventoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
